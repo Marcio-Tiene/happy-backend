432be1f464fa2a87bd814dd7f3e3e3f205a49115
Marcio Tiene <marciorft@gmail.com>
2020-10-15 12:06:14 -0300

    Post orphanages init

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,41 @@
 import express from 'express';
 
+import { getRepository } from 'typeorm';
+
+import { Orphanage } from './models/Orphanage';
+
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello Wolrd' });
-});
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
 
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json({ message: 'Orphanage saved' });
+});
+console.log('The server is running');
 app.listen(3333);
