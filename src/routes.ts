9482e3ad21ca51a62de3699c3fc9aeb48f0c2032
Marcio Tiene <marciorft@gmail.com>
2020-10-15 22:05:07 -0300

    Creating orphanages with images

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphanagesController from './controllers/OrphanagesController';
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
+routes.get(
+  '/orphanages/:id',
+  upload.array('images'),
+  OrphanagesController.show
+);
+
+export default routes;
